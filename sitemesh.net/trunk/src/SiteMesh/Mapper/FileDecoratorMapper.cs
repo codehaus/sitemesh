eae341e52e0e6b7bb09595fd9b6f1011e542eb9f
lahma <lahma@6bb08542-a8e7-0310-afb1-f65034787f60>
2004-12-28 21:24:27 +0000

    First (non-working) translations

---
+++ sitemesh.net/trunk/src/SiteMesh/Mapper/FileDecoratorMapper.cs
@@ -0,0 +1,67 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace SiteMesh.Mapper
+{
+	/// <summary>
+	/// The FileDecoratorMapper will treat the name of the decorator as a file-name to use
+	/// (in the context of the web-app).
+	///	@author <a href="joe@truemesh.com">Joe Walnes</a>
+	/// @author <a href="mike@atlassian.com">Mike Cannon-Brookes</a>
+	/// </summary>
+	public class FileDecoratorMapper : AbstractDecoratorMapper 
+	{
+		private static bool pathNotAvailable = false;
+
+		public override IDecorator GetNamedDecorator(HttpRequest request, string name)
+		{
+			if (pathNotAvailable || name == null) 
+			{
+				return base.GetNamedDecorator(request, name);
+			}
+
+			string resourcePath = null;
+
+			// try to locate the resource (might be an unexpanded WAR)
+			try  
+			{
+				// ?? resourcePath = request.MapPath(name);
+			}
+			catch
+			{
+				//e.printStackTrace();
+				// return super.getNamedDecorator(req, name);
+			}
+
+			string filePath = request.MapPath(name);
+
+			if (filePath == null && resourcePath == null) 
+			{
+				pathNotAvailable = true;
+				return base.GetNamedDecorator(request, name);
+			}
+			else if (filePath != null) 
+			{ 
+				// do we really need this disk file check?!
+				if (File.Exists(filePath)) 
+				{
+					// if filename exists with name of supplied decorator, return Decorator
+					return new DefaultDecorator(name, name, null);
+				}
+				else 
+				{
+					// otherwise delegate to parent mapper.
+					return base.GetNamedDecorator(request, name);
+				}
+			}
+			else 
+			{
+				// file path is null and resource path is not null - can't check file on disk
+				return new DefaultDecorator(name, name, null);
+			}
+		}
+		
+	}
+}
---
+++ sitemesh.net/trunk/src/SiteMesh/Mapper/LanguageDecoratorMapper.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections;
+using System.IO;
+using System.Web;
+
+namespace SiteMesh.Mapper
+{
+	/// <summary>
+	/// The LanguageDecoratorMapper can determine the preferred language set in the
+	/// browser requesting a page, and map to a suitable Decorator (using the
+	/// "Accept-Language" HTTP header).
+	///
+	/// <p>This can be useful for supplying different versions of the same content
+	/// for different languages.</p>
+	///
+	/// <p>When LanguageDecoratorMapper is in the chain, it will request the appropriate Decorator
+	/// from its parent. It will then add an extention to the filename of the Decorator, and
+	/// if that file exists it shall be used as the Decorator instead. For example, if the
+	/// Decorator path is <code>/blah.jsp</code> and the detected preferred language is <code>en</code>,
+	/// the path <code>/blah-en.jsp</code> shall be used.</p>
+	///
+	/// <p>The language mappings are configured by passing properties with <code>match.</code> as a prefix.
+	/// For example: 'match.en'=engl , 'match.nl'=dutch .</p>
+	///
+	/// @author <a href="mailto:pathos@pandora.be">Mathias Bogaert</a>
+	/// </summary>
+	public sealed class LanguageDecoratorMapper : AbstractDecoratorMapper 
+	{
+		private IDictionary map = null;
+
+		public override void Init(IDictionary properties, IDecoratorMapper parent)
+		{
+			base.Init(properties, parent);
+			map = new Hashtable();
+			InitMap(properties);
+		}
+
+		public override IDecorator GetDecorator(HttpRequest request, IPage page) 
+		{
+			try 
+			{
+				IDecorator result = null;
+				IDecorator d = base.GetDecorator(request, page);
+				string path = ModifyPath(d.Page, GetExt(request.Headers["Accept-Language"]));
+
+				//File decFile = null; //new File(request.Server.MapPath(path));
+
+				/*if (decFile.isFile()) {
+					result = new DefaultDecorator(d.getName(), path, null) {
+						public string getInitParameter(string paramName) {
+							return d.getInitParameter(paramName);
+						}
+					};
+				}*/
+				return result == null ? base.GetDecorator(request, page) : result;
+			}
+			catch 
+			{
+				return base.GetDecorator(request, page);
+			}
+		}
+
+		/** Get extention for the language. */
+		private string GetExt(string acceptLanguage) 
+		{
+			foreach (string key in map.Keys) {
+
+			    // Get the first language (preferred one) in the header, and
+				// only check the first two chars (the acceptLanguage could be en-gb, but
+				// we don't support this for now).
+				if (acceptLanguage.Substring(0, 2).Equals(key)) {
+					return (string) map[key];
+				}
+			
+
+				// When the user-agent has multiple accept-languages (separated by ;),
+				// these are ignored because the site creator may wish that if the
+				// preferred language is not supported, the page uses the default
+				// decorator (in the default language), and not in some other
+				// language given by the browser (most of them are specified by
+				// default at install).
+			}
+			return null;
+		}
+
+		/** Change /abc/def.jsp into /abc/def-XYZ.jsp */
+		private static string ModifyPath(String path, String ext) 
+		{
+			int dot = path.IndexOf('.');
+			if (dot > -1) 
+			{
+				return path.Substring(0, dot) + '-' + ext + path.Substring(dot);
+			}
+			else 
+			{
+				return path + '-' + ext;
+			}
+		}
+
+		/** Initialize language mappings. */
+		private void InitMap(IDictionary props) 
+		{
+			ICollection keys = props.Keys;
+			foreach (string key in keys) {
+
+				if (key.StartsWith("match.")) {
+					string match = key.Substring(6);
+					map.Add(match, props[key]);
+				}
+			}
+		}
+	}
+}
